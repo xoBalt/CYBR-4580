b8ba8541acf6b849382f531000c5a1e6a5b41ff7
DESKTOP-DCGC4IM\benba <gbaltzer@unomaha.edu>
2019-03-06 17:20:20 -0600

    Server code for single socket communication.

--- Code/interfaceEnumerator.py
+++ Code/interfaceEnumerator.py
@@ -1,9 +1,11 @@
 import ifaddr
+import socket
 
 adapters = ifaddr.get_adapters()
 
 for adapter in adapters:
     if "VMware" not in adapter.nice_name:
-        print ("IPs of network adapter " + adapter.nice_name)
+        #adapter.name is the actual name of the adapter. In linux this would appear as "eth0" and so on.
+        print ("IPs of network adapter " + adapter.name.decode("utf-8") )
         for ip in adapter.ips:
             print ("   %s/%s" % (ip.ip, ip.network_prefix))
