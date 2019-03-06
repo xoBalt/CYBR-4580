7c969dbbb0023d393161c0d1162e795ab691da7d
DESKTOP-DCGC4IM\benba <gbaltzer@unomaha.edu>
2019-03-06 17:32:48 -0600

    Simple client server.

--- Code/client.py
+++ Code/client.py
@@ -1,7 +1,7 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 50000))
-s.sendall('Hello, world')
+s.sendall(b'Hello, world')
 data = s.recv(1024)
 s.close()
-print 'Received', repr(data)
+print ('Received', repr(data))
